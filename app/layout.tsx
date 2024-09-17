512ee4e694dd2bc2e4f721d7f8674875b646b59a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-09-17 12:00:27 +0330

    initialize

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
