53d072f3ab5e24ce0cddd5b19ee33f4e65b4e09a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-09-17 15:47:06 +0330

    folder structor

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default RootLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
       >
         {children}
       </body>
