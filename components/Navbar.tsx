1d55ba3ebeca260c2321c9636a0a5ed9388dc0b4
AliSabet81 <sabet.farshid1381@gmail.com>
2024-09-18 10:30:25 +0330

    mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href={"/"} className="flex items-center gap-1">
         <Image
           src={"/icons/logo.svg"}
@@ -18,7 +18,7 @@ const Navbar = () => {
           Yoom
         </p>
       </Link>
-      <div className="flex justify-between gap-5">
+      <div className="flex-between gap-5">
         {/* Clerk */}
         <MobileNav />
       </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link
