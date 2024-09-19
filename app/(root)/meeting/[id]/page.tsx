3b5cbeef3812fc8003cd0873a9e55426c782c753
AliSabet81 <sabet.farshid1381@gmail.com>
2024-09-19 08:38:23 +0330

    starting meeting room

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
 const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room {params.id}</div>;
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  return (
+    <main className="h-screen w-full">
+      <StreamCall
+      //  call={}
+      >
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
