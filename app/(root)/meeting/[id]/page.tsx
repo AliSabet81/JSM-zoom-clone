f2a7dcc158d39810de8db1d9910003d1e6dd9106
AliSabet81 <sabet.farshid1381@gmail.com>
2024-09-25 15:09:06 +0330

    adding alert

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Alert from "@/components/Alert";
 import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
@@ -13,6 +14,21 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { call, isCallLoading } = useGetCallById(id);
 
   if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed =
+    call.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
